acc7f69d5ae9edbb38a378f9967b086a73071658
AdiNairCS50 <66921216+AdiNairCS50@users.noreply.github.com>
2022-01-04 13:11:50 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball
+
+var ball_options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	density:1.2
+}
+
+function preload(){
+	groundObj=new ground(width/2,670,width,20);
+	leftSide=new ground(1100,600,20,120);
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	Matter.Bodies.circle(1100,600,2);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj.display();
+  drawSprites();
+ 
+}
+
+function keyPressed()
+{
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(circle, circle.position, 1);
+	}
+}
+
